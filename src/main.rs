54376632c1cb17bff46c2fb6ead2009f3656756c
Jack Contro <jack.contro@gmail.com>
2023-01-17 11:48:06 +0000

    Refactoring with error managment

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_configs(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
-        
-    println!("With text: \n{}", contents);
+    
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        std::process::exit(1);
+    }
 }
 
 struct Config {
@@ -18,8 +23,21 @@ struct Config {
     filename: String,
 }
 
-fn parse_configs(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text: \n{}", contents);
+    Ok(())
 }
