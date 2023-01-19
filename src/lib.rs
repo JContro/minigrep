fbfb9980213a922787e84f7378498dc505006799
Jack Contro <jack.contro@gmail.com>
2023-01-19 10:44:15 +0000

    simplifying searhc with a iterator and closure

--- src/lib.rs
+++ src/lib.rs
@@ -51,15 +51,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
